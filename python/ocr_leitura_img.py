a9f4574f027ab4cc6e3bb5a9a56689d0a273b87c
pedro <pedro.domingues@quaestum.com.br>
2024-08-14 16:06:24 -0300

    ajustando comentarios do codigo

--- python/ocr_leitura_img.py
+++ python/ocr_leitura_img.py
@@ -3,13 +3,14 @@ import re
 
 def LeituraDocumento():
     caminhos = [
-        r"nome do arquivo pdf que ira ler",
-        r"nome do arquivo pdf que ira ler"
+        r"caminho do arquivo pdf que voce ira ler",
+        r"caminho do arquivo pdf que voce ira ler"
     ]
     for caminho in caminhos:
         pdf_document = fitz.open(caminho)
         page = pdf_document.load_page(0)
     
+        # ajuste as coordenadas de acordo com o campo que voce quer pegar no arquivo
         coordenadas = fitz.Rect(500, 320, 565, 327)   
         text = page.get_text("text", clip=coordenadas)
     
